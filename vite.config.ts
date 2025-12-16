3362a1b0e17ad9bb1c473ce93eece1b9b39cc3ab
Benjamin Navarro Salinas <benjaminns_l@outlook.es>
2025-12-16 17:51:59 -0500

    fix: genera las páginas correspondientes para ser visibles en la nube

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = resolve(__filename, '..')
 
 export default defineConfig({
   base: '/welcome-page/',
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        pagina1: resolve(__dirname, 'paginas/animation_portfolio/index.html'),
+      }
+    }
+  }
 })
